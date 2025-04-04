f9fc798f071b1afa9d4b605cb65522ee4d414a8d
jjhjahd69 <kakogohulio@gmail.com>
2025-04-04 20:25:41 +0300

    - Додано приклад конфігу

---
+++ config_template.py
@@ -0,0 +1,17 @@
+# Конфігураційний файл
+# НЕОБХІДНО ПЕРЕЙМЕНУВАТИ У config.py
+
+# Базові дані авторизації
+TOKEN = ''
+BOT = ""
+ID = None # int Application ID 
+PREFIX = ""
+ALLOWED_GUILD = None # Якщо необхідно обмежити певним дискорд сервером
+MODERATOR_LIST = [] # userid модераторів через кому
+
+# Дані підключення до MYSQL
+db_host = "127.0.0.1" # або ip сервера, якщо бот НЕ там же де база
+db_port = 3306 # або інший порт
+db_user = "root"
+db_password = "" 
+db_name = ""
